058b6942195abcd35f7b98611c198e880f501d75
zireael9797 <ryanhossain9797@gmail.com>
2021-01-08 00:27:37 +0600

    Pattern matching

--- Program.fs
+++ Program.fs
@@ -2,8 +2,21 @@
 
 open System
 
+type Shape =
+    | Rectangle of Width: double * Height: double
+    | Triangle of Side1: double * Side2: double * Side3: double
+    | Circle of Radius: double
+    | Dot
+
+let area =
+    function
+    | Rectangle (w, h) -> w * h
+    | Triangle (a, b, c) -> sqrt ((a + b + c) * (b + c) * (c + a) * (a + b))
+    | Circle (r) -> Math.PI * r * r
+    | Dot -> 0.
+
 [<EntryPoint>]
 let main argv =
-    let person = argv.[0]
-    printfn "Hello %s" person
+    let triangle = Triangle(1., 2., 3.)
+    printfn "%f" (area triangle)
     0 // return an integer exit code
